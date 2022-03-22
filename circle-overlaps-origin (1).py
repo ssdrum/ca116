2b614637c36247dea2557e100ae3bfeadd5b1c07
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2022-03-22 00:26:43 +0000

    removed duplicates

--- circle-overlaps-origin (1).py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-x = int(input())
-y = int(input())
-r = int(input())
-
-print(((0 - x) * (0 - x) + (0 - y) * (0 - y) ) <= r * r)
-
-
--- counting-down (1).py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-n = int(input())
-
-i = 0
-while i < n:
-    print(n - i)
-    i = i + 1
